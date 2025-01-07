da9c2c74b14485d12743d3d78d37351d17760113
Rodolfo Rolon <kelzemrod@gmail.com>
2025-01-07 20:37:20 +0100

    Small agenda, all options(add, delete and search people added)

--- project3.py
+++ project3.py
@@ -10,11 +10,15 @@ def add_person():
     }
     return person
 
-def delete_person(people):
+def display_people(people):
     for i, person in enumerate(people):
-        print(i + 1, " -" , person["name"], "|", person["age"], "|", person["email"])
+        print(i + 1, " - " , person["name"], "|", person["age"], "|", person["email"])
+
+def delete_person(people):
+    
+    display_people(people)
         
-        while True:
+    while True:
             number = input("Enter a number to delete: ")
             try:
                 number = int(number)
@@ -25,29 +29,44 @@ def delete_person(people):
             except:
                 print("Invalid number")
         
-        people.pop(number -1)
-        print("Person deleted")
+    people.pop(number -1)
+    print("Person deleted")
+        
+def search(people):
+    search_name = input("Search for a name: ").lower()
+    results = []
+    
+    for person in people:
+        name = person["name"]
+        if search_name in name.lower():
+            results.append(person)
+    
+    display_people(results)
 
-#INICIO:
+#START:
 print("Hi welcome to my agenda! ")
 print("")
 
-people = []
+#LIST OF PEOPLE
+people = [
+    {"name": "Kelu", "age": "27", "email": "rodolfomews@gmail.com"},
+    {"name": "Nat", "age": "25", "email": "natalinerja@gmail.com"}
+    ]
 
 while True:
     print("Contact list size: ",  len(people))
-    command = input("You can 'ADD', 'DELETE' or 'SEARCH'. Press 'q' for quit: ").lower()
+    command = input("You can 'ADD(a)', 'DELETE(d)' or 'SEARCH(s)'. Press 'q' for quit: ").lower()
 
     
 
-    if command == "add":
+    if command == "a":
         person = add_person()
         people.append(person)
         print("Perdon added")
-    elif command == "delete":
+    elif command == "d":
         delete_person(people)
-    elif command == "search":
-        pass
+    elif command == "s":
+        search(people)
     elif command == "q":
         print("See you later!")
         break
